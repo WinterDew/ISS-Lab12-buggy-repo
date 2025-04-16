6c7d195345dc1a8a54a70f98e10bb87c3d3c4cf6
Tatva Agarwal <tatva.agarwal.2006@gmail.com>
2025-04-16 15:54:34 +0530

    Fix server

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 from routes.items import router as items_router
 from routes.analytics import router as analytics_router
 from routes.quiz import router as quiz_router
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -13,8 +14,8 @@ app.include_router(analytics_router)
 app.include_router(quiz_router)
 
 # Set up templates directory
-templates = Jinja2Templates(directory="../frontend")
+app.mount("/", StaticFiles(directory="../frontend", html=True), name="frontend")
 
-@app.get("/home", response_class=HTMLResponse)
-async def get_home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+# @app.get("/home", response_class=HTMLResponse)
+# async def get_home(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
